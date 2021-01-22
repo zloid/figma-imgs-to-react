65a88498ac50ea70a645fe8035464e4280188aa3
zloid <zloid-1@yandex.by>
2021-01-22 11:48:47 +0100

    Updates

---
+++ precache-manifest.f638dcc8b1bd107701fe17961d9f3dd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad51643325d97e3ccf7365346d67040",
+    "url": "/figma-template-to-react/index.html"
+  },
+  {
+    "revision": "5905506259bda50adc9e",
+    "url": "/figma-template-to-react/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "a4fdbb1bbbd3dbb53760",
+    "url": "/figma-template-to-react/static/css/main.2d41e40e.chunk.css"
+  },
+  {
+    "revision": "5905506259bda50adc9e",
+    "url": "/figma-template-to-react/static/js/2.36dd3e38.chunk.js"
+  },
+  {
+    "revision": "a4fdbb1bbbd3dbb53760",
+    "url": "/figma-template-to-react/static/js/main.d1117b9c.chunk.js"
+  },
+  {
+    "revision": "3d3df5adceaeeecae2c1",
+    "url": "/figma-template-to-react/static/js/runtime-main.fad71c43.js"
+  }
+]);
